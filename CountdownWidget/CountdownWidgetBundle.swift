35e48820da8eb498ca1d2437d0ebaa9366781acc
Vladimir Kudryashov <doctorinternet@icloud.com>
2026-02-28 12:51:35 +0100

    Add files via upload

---
+++ CountdownWidget/CountdownWidgetBundle.swift
@@ -0,0 +1,11 @@
+//import WidgetKit
+//import SwiftUI
+//
+//@main
+//struct CountdownWidgetBundle: WidgetBundle {
+//    var body: some Widget {
+//        CountdownWidget()
+//        CountdownWidgetControl()
+//        CountdownWidgetLiveActivity()
+//    }
+//}
